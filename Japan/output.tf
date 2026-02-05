41c6aaa64bdd04add478ed5947b67025eef78bcb
7twoduo <152588192+7twoduo@users.noreply.github.com>
2026-02-05 18:32:59 -0500

    Update outputs in output.tf for VPC and transit gateway

--- Japan/output.tf
+++ Japan/output.tf
@@ -4,8 +4,11 @@ output "region" {
   value = data.aws_region.current.region
 }
 output "vpc_cidr" {
-  value = var.vpc_cidr
+  value = aws_vpc.Star.cidr_block
 }
 output "Ec2_transit_gateway" {
-  value = aws_ec2_transit_gateway.shinjuku_tgw01
+  value = aws_ec2_transit_gateway.shinjuku_tgw01.id
 }
+# output "Ec2_transit_gateway" {
+#   value = aws_db_instance.below_the_valley.address
+# }
